19a30e56a6566ecb1ae00e9ae8953e15ec6fbd33
Techmarket12 <96696189+Techmarket12@users.noreply.github.com>
2025-06-23 23:26:56 +0200

    Update HeroSection.tsx

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -48,7 +48,7 @@ export default function HeroSection() {
               <div className="space-y-6">
                 <h1 className="text-4xl lg:text-6xl font-bold leading-tight">
                   <span className="text-[#849FE1] block">Votre Habitat</span>
-                  <span className="text-[#849FE1] block">vous protège</span>
+                  <span className="text-[#849FE1] block">vous protège &</span>
                   <span className="text-[#59D14C] block">Nous protégeons</span>
                   <span className="text-[#59D14C] block">votre Habitat</span>
                 </h1>
